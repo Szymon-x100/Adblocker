81c60a5132e90c6770b7f846a9731ec3a0c3a2a9
SZYMDEV <167322257+Szymon-x100@users.noreply.github.com>
2024-11-01 20:38:05 +0100

    Update background.js

--- AD-BLOCKER/background.js
+++ AD-BLOCKER/background.js
@@ -1,6 +1,6 @@
 chrome.webRequest.onBeforeRequest.addListener(
    function(details) { 
     return { cancel: true };
-   },{urls: ["*://*.zedo.com/*", "*://*.publicis.com/*", "*://*.wpp.com/*", "*://*.adfox.ru/*", "*://*.omnicomgroup.com/*", "*://*.dentsu.com/*", "*://*.havas.com/*", "*://*.adx.com/*", "*://*.adx.com/*"]},
+   },{urls: ["*://*.zedo.com/*", "*://*.publicis.com/*", "*://*.wpp.com/*", "*://*.adfox.ru/*", "*://*.omnicomgroup.com/*", "*://*.dentsu.com/*", "*://*.havas.com/*", "*://*.adx.com/*"]},
    ["blocking"]
 );
